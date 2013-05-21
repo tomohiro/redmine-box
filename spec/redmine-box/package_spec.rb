c3465442bc9424821677081f332bdc206d906186
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-05-21 16:23:41 +0900

    update serverspec v0.4.8

--- spec/redmine-box/package_spec.rb
+++ spec/redmine-box/package_spec.rb
@@ -1,24 +1,28 @@
 require 'spec_helper'
 
-describe :Packages do
-  describe 'postgresql' do
-    it { should be_installed }
-    it { should be_running }
-    it { should be_enabled }
-  end
+describe package('postgresql') do
+  it { should be_installed }
+end
 
-  describe 'apache2' do
-    it { should be_installed }
-    it { should be_running }
-    it { should be_enabled }
-  end
+describe service('postgresql') do
+  it { should be_running }
+  it { should be_enabled }
+end
 
-  describe 'git' do
-    it { should be_installed }
-  end
+describe package('apache2') do
+  it { should be_installed }
+end
 
-  describe '/usr/local/bin/ruby-build' do
-    it { should be_file }
-    it { should be_executable }
-  end
+describe service('apache2') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe package('git') do
+  it { should be_installed }
+end
+
+describe file('/usr/local/bin/ruby-build') do
+  it { should be_file }
+  it { should be_executable }
 end
